d53920e807c45cfc634dc1b5e901f44e31267463
Sarath Chandra Dhanalakota <dschandra2003@gmail.com>
2025-02-22 15:57:47 +0530

    jwt token

--- src/app.js
+++ src/app.js
@@ -3,16 +3,76 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const {validateSignUpData} = require("./utils/validation");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const {userAuth} = require("./middlewares/auth");
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", async (req,res)=>{
-    const user = new User(req.body);
+    
     try{
+        validateSignUpData(req);
+
+        const {firstName, lastName, emailId, password} = req.body;
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash,
+        });
         await user.save();
         res.send("User saved to database successfully");
     }catch(err){
-        res.status(400).send("Error in saving data to DB"+err.message);
+        res.status(400).send("Error : "+err.message);
+    }
+});
+
+app.post("/login",async (req,res)=>{
+    try{
+        const {emailId, password} = req.body;
+        const user = await User.findOne({emailId: emailId});
+        if(!user){
+            throw new Error("Invalid credentials");
+        }
+
+        const isPasswordValid = await user.validatePassword(password);
+        if(isPasswordValid){
+            const token = await user.getJWT();
+
+            res.cookie("token", token, {
+                expires: new Date(Date.now() + 8 * 3600000),
+            });
+
+            res.send("Login successfully!!!");
+        }else{
+            throw new Error("Invalid credentials");
+        }
+       
+
+    }catch(err){
+        res.status(400).send("Error : "+err.message);
+    }
+
+});
+
+app.post("/sendConnectionRequest", userAuth, async(req,res)=>{
+    const user = req.user;
+    res.send(user.firstName + " sent connection request.");
+})
+
+app.get("/profile", userAuth, async (req,res)=>{
+    try{
+        const user = req.user;
+        res.send(user);
+    }catch(err){
+        res.status(400).send("Error : "+err.message);
     }
 });
 
