ee975a379ad5d7cd8fd563c84d5bbaff6663f6be
Sarath Chandra Dhanalakota <dschandra2003@gmail.com>
2025-03-09 17:12:45 +0530

    FrontEnd supports

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -19,8 +19,14 @@ authRouter.post("/signup", async (req,res)=>{
             emailId,
             password: passwordHash,
         });
-        await user.save();
-        res.send("User saved to database successfully");
+        const savedUser = await user.save();
+        const token = await savedUser.getJWT();
+    
+        res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+        });
+    
+        res.json({ message: "User Added successfully!", data: savedUser });
     }catch(err){
         res.status(400).send("Error : "+err.message);
     }
